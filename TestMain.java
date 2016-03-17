1db14acfbd719e3c950670fd18d1358d1e793e09
MrHanSan <hanssandbu@gmail.com>
2016-03-17 13:33:35 +0100

    Delete TestMain.java

--- TestMain.java
+++
@@ -1,9 +0,0 @@
-import daoimpl.PersonImpl;
-
-public class TestMain {
-    public static void main(String[] args) {
-        PersonImpl pdi = new PersonImpl();
-
-        pdi.createPersonTable();
-    }
-}
